355a61ea5a1a83a7b863b94f3f91f7476c81983e
arnab <arnabthakuria678@gmail.com>
2024-01-03 20:11:48 +0530

    adding all the stages

--- main.go
+++ main.go
@@ -1,49 +1,99 @@
 package main
 
-
 import (
-  "flag"
-  "fmt"
-  "os"
-  "strings"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
 func main() {
-    charCount := flag.Bool("c", false, "parse byte counts")
-    lineCount := flag.Bool("l", false, "get line counts")
-    wordCount := flag.Bool("w", false , "get word counts")
-    flag.Parse()
-    tails := flag.Args()
-    fileName := tails[0]
-     
-   
-    content , err := os.ReadFile(fileName)
-    if err!=nil{
-      fmt.Println(err)
-      os.Exit(-1)
-    }
-    if *charCount {
-        fmt.Fprintf(os.Stdout, "%d %s\n", len(content), fileName)
-    }
-    if *lineCount {
-        newLine := byte('\n') 
-        count := 0 
-    for _ , val := range content {
-      if val == newLine {
-        count++
-      } 
-    }
-    fmt.Fprintf(os.Stdout, "%d %s\n",count, fileName)
-    }
+	byteCount := flag.Bool("c", false, "parse byte counts")
+	lineCount := flag.Bool("l", false, "get line counts")
+	wordCount := flag.Bool("w", false, "get word counts")
+	charCount := flag.Bool("m", false, "get byte count")
+	flag.Parse()
+	tails := flag.Args()
 
-    if *wordCount {
-      count := 0 
-      strContent := string(content)
-      for _, line := range strings.Split(strContent , "\n"){
-        count += len(strings.Fields(line))
-      }
-      fmt.Fprintf(os.Stdout, "%d %s\n", count, fileName)
-    }
+	content := []byte{}
+	fileName := ""
 
+	if len(tails) == 0 {
+		ioContent, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			panic(err)
+		}
+		content = ioContent
+		// no file name is specified . try reading from stdin
+	} else {
 
+		fileName = tails[0]
+		fileContent, err := os.ReadFile(fileName)
+		content = fileContent
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	defaultOpt := true
+	if *byteCount {
+		result := CountBytes(content)
+		fmt.Fprintf(os.Stdout, "%d %s\n", result, fileName)
+		defaultOpt = false
+	}
+	if *lineCount {
+		result := CountLines(content)
+		fmt.Fprintf(os.Stdout, "%d %s\n", result, fileName)
+		defaultOpt = false
+	}
+
+	if *wordCount {
+		result := CountWords(content)
+		fmt.Fprintf(os.Stdout, "%d %s\n", result, fileName)
+		defaultOpt = false
+	}
+	if *charCount {
+		result := CountChars(content)
+		fmt.Fprintf(os.Stdout, "%d %s\n", result, fileName)
+		defaultOpt = false
+	}
+
+	if defaultOpt {
+		byteCount := CountBytes(content)
+		lineCount := CountLines(content)
+		wordCount := CountWords(content)
+		fmt.Fprintf(os.Stdout, "  %d %d %d %s\n", lineCount, wordCount, byteCount, fileName)
+
+	}
+
+}
+
+func CountBytes(content []byte) int {
+	return len(content)
+
+}
+func CountLines(content []byte) int {
+	newLine := byte('\n')
+	count := 0
+	for _, val := range content {
+		if val == newLine {
+			count++
+		}
+	}
+	return count
+
+}
+func CountWords(content []byte) int {
+	count := 0
+	strContent := string(content)
+	for _, line := range strings.Split(strContent, "\n") {
+		count += len(strings.Fields(line))
+	}
+	return count
+
+}
+func CountChars(content []byte) int {
+	strContent := string(content)
+	return len(strContent)
 }
---
+++ main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestCountBytes(t *testing.T) {
+
+}
+
+func TestCountLines(t *testing.T) {
+
+}
+
+func TestCountWords(t *testing.T) {
+
+}
+
+func TestCountChars(t *testing.T) {
+
+}
